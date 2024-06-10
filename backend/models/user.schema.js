1167c6dbc6a434fe2d52923f15d56dab2c0bc938
Vicky91661 <kumar.vickysah72@gmail.com>
2024-06-10 15:48:37 +0530

    added user schema 1

--- backend/models/user.schema.js
+++ backend/models/user.schema.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles"
+import bcrypt from "bcryptjs"
+import JWT from "jsonwebtoken"
+import config from "../config/index"
 
 //define the user Schema . The data you want to store for the user. 
 const userSchema = mongoose.Schema({
@@ -29,5 +32,35 @@ const userSchema = mongoose.Schema({
 },{
     timestamps:true
 })
+
+
+//ecrypt password hooks
+userSchema.pre("save",async function(next){
+    if(!this.modified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10) // 10 rounds
+    next()
+})
+
+//add more features directly to your schema
+
+userSchema.methods = {
+    //compare password
+    comparePassword:async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword,this.password);
+    },
+
+    // generate JWT Token
+    getJwtToken:function(){
+        return JWT.sign({
+            _id:this._id,
+            role:this.role
+            },
+            config.JWT_SECRET,
+            {
+            expiresIn:config.JWT_EXPIRY
+            }
+        )
+    }
+}
 
 export default mongoose.model("User",userSchema)
